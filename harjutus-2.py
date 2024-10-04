56022938af5b0f79ebcaf26a83c90494b1486fa2
Alex Kreimann <alex@hkhk.edu.ee>
2024-10-04 11:40:20 +0300

    Add files via upload

---
+++ harjutus-2.py
@@ -0,0 +1,14 @@
+#Alex Kreimann
+#04.10.24
+#Harjutus02
+
+#9. Arvusüsteem
+arv=int(input("Lisa 10nd arv: "))
+pprint(bin(arv))
+print(hex(arv))
+
+#10. Kütusekulu arvutamine
+liiter=int(input("Lisa kütusekul: "))
+km=int("Lisa läbitud kilomeetrid: "))
+kytusekulu=liiter/(km/100)
+print("Sinu keskmine kütusekulu on: ",kytusekulu)
