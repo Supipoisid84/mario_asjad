14b905655feb9e506eadf79e757a1fd3687c72ba
Alex Kreimann <alex@hkhk.edu.ee>
2024-10-04 11:19:50 +0300

    Add files via upload

---
+++ harjutus-1.py
@@ -0,0 +1,17 @@
+#Alex Kreimann
+#04.10.2024
+#Harjutus01
+
+print('"Cabernet Sauvignon" või \'Cabernet Sauvignon\'?')
+print('  @..@')
+print(' (----)')
+print('( \__/ )')
+print('^^ "" ^^ ')
+
+#int täisarv 123
+#str sõne "tekst"
+#float komaarv 1.23
+
+a=int(input("Lise ruudu külje pikkus: "))
+s=a**2#astendamine
+print("Vastus:ruudu pindala on",s)
