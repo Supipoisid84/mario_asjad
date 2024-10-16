5b8e299462e54c107602290e85e32b80f6c5ae9e
Alex Kreimann <alex@hkhk.edu.ee>
2024-10-16 11:46:14 +0300

    Create ylesanne-4.py

---
+++ ylesanne-4.py
@@ -0,0 +1,16 @@
+#Alex Kreimann
+#16.10.2024
+#Ülesanded04
+
+#2. Raamatute allahindlus
+ale=0.3
+arv=3
+hind=12.53
+summa=hind-(hind*ale)*arv
+print(f"{arv} raamatu hind soodustusega on {summa}€")
+
+#1. Aia pikkus
+a=(input("Sisesta külg 1: "))
+b=(input("Sisesta külg 2: "))
+p=2*(2+b)
+print(f"Aia kogupikkus on {p} meetrit")
