e52e037f375fa52a01137a4cfe4c72e73b47b486
Alex Kreimann <alex@hkhk.edu.ee>
2024-10-16 11:30:25 +0300

    Create ylesanne-3.py

---
+++ ylesanne-3.py
@@ -0,0 +1,35 @@
+#Alex Kreimann
+#16.10.2024
+#Ülesanded03
+
+import turtle
+
+#Ülesanne3.2:Ostunimekiri
+
+#Ülesanne3.4:Lemmikraamat
+
+#Ülessane3.5:Unistuste auto
+mark, mudel="volskwagen", "tüüp1"
+auto=mark+" "+mudel
+aasta=1938
+hind=18500
+print("Minu unistuste auto on", auto,", mille hind on ", hind, "eurot.")
+
+"""
+#Ülesanne3.6:Python Turtle kolmnurk
+kylie_pikkus=200
+nurk=120
+kujundi_varv="red"
+
+turtle.color(kujundi_varv)
+
+for i in range(3):
+    for i in range(3):
+        turtle.forward(kylie_pikkus)
+        turtle.left(nurk)
+
+    turtle.penup()
+    turtle.forward(kylie_pikkus*1.5)
+    turtle.pendown()
+"""
+turtle.done()
