9466bf878599457ecb87fdfb422f4cbe0b88c625
Alex Kreimann <alex@hkhk.edu.ee>
2024-10-22 11:11:14 +0300

    Update ylesanne-6.py

---
+++ ylesanne-6.py
@@ -0,0 +1,30 @@
+#Alex Kreimann
+#18.10.2024
+#Ülesanded06
+
+import turtle
+import math
+
+a=math.radians(53)
+h=4.4
+x=abs(h/math.tan(a))
+c=math.sqrt(h**2+**2)
+
+print(x)
+
+turtle.forward(x*kordaja)
+turtle.left(90)
+turtle.forward(h*kordaja)
+turtle.left(180-(180-(180-90-53)))
+turtle.forward(c*kordaja)
+
+
+
+
+
+
+
+
+
+
+turtle.done()
