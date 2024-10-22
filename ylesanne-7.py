bfda4053c84a1055fd71965647402da591719faa
Alex Kreimann <alex@hkhk.edu.ee>
2024-10-22 11:11:17 +0300

    Create ylesanne-7.py

---
+++ ylesanne-7.py
@@ -0,0 +1,9 @@
+#Alex Kreimann
+#22.10.2024
+#Ülesanded07 loendid
+
+muusika=[
+'ALIKA – "Bridges"','Anett x Fredi – "Read Between The Lines"','villemdrillem – "leekiv armastus"','Clicherik & Mäx – "PAKSUD"','nublu – "ära ärata"','NOËP – "Move Your Feet"',
+'Trad.Attack! – "Bring It On"','Bedwetters – "It Is What It Is"','Reket – "Panama paberid"','Grete Paia – "Võluväel"']
+
+print(muusika[0])
