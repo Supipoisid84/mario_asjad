a47c75c67895b1d3cf6cb07259f853c70079c9cc
Alex Kreimann <alex@hkhk.edu.ee>
2024-11-15 11:46:16 +0200

    Update ylesanne-11.py

---
+++ ylesanne-11.py
@@ -0,0 +1,100 @@
+#Alex Kreimann
+#08.11.2024
+#Ülesanne11
+
+import turtle
+import random
+
+"""
+def pikim_sona(*sonad):
+    pikim=0
+    for i in sonad:
+        if len(1)>pikim:
+            pikim=len(i)
+    print(f"Pikim sõna on {pikim} märki!")
+
+pikim_sona("üks","kaks","kolmkümmend")
+"""
+
+"""
+def kolm_pikimat_sona(a):
+    sonastik={}
+    for i in a:
+        sonastik[i]=len(i)
+    print(sorted(sonastik.items(),key=lambda x:x[1],reverse=True))
+for j in range(3):
+    print(sorteeritud[j])
+
+sonad=["üks","kaks","viis","kolmsada","üksmiljonsadamust"]
+kolm_pikimat_sona(sonad)
+"""
+
+"""
+def sarnased_esitahed(nimi):
+    tykk=nimi.split(" ")
+    if tykk[0][0]==tykk[1][0]:
+        return True
+    return tykk[1][0]
+
+print(sarnased_esitahed("Vapper Vares"))
+print(sarnased_esitahed("Lahe Känguru"))
+"""
+
+def viisnurk(k):
+    screen = turtle.Screen()
+screen.title('Pentagram Fractal - PythonTurtle.Academy')
+screen.setup(1000,1000)
+screen.setworldcoordinates(-1000,-1000,1000,1000)
+turtle.speed(0)
+turtle.hideturtle()
+
+def star(x,y,direction,r): #x,y is the center
+    turtle.up()
+    turtle.goto(x,y)
+    turtle.seth(direction)
+    turtle.fd(r)
+    turtle.right(180-18)
+    turtle.down()
+    length = 2*r*math.sin(math.pi*2/5)
+    for _ in range(5):
+        turtle.fd(length)
+        turtle.right(180-36)
+
+def star_fractal(x,y,direction,r):
+    star(x,y,direction,r)
+    if r < 50: return
+    star_fractal(x,y,180+direction,r*math.sin(math.pi/10)/math.cos(math.pi/5))
+
+
+star_fractal(0,0,90,1000)
+
+def ring(k):
+    pass
+
+def ruut(k):
+    turtle.speed(0)
+    for j in range(k):
+        turtle.penup()
+        turtle.goto(random.randint(-400,400),random.randint(-400,400))
+        turtle.pendown()
+        turtle.rt(random.randint(0,90))
+        for i in range(4):
+            turtle.forward(100)
+            turtle.left(90)
+def suvaline(k):
+    for i in range(k):
+        random.choice([viisnurk(1),ring(1),ruut(1)])
+
+print("----------Minu rõve programm----------")
+valik=int(input("1 - viisnurk\n2 - ring \n3 - ruut\n4 - suvaline\nLisa valik (1-4): "))
+kujunditeArv=int(input("Mitu kujundit soovid joonistada: "))
+if valik==1:
+    viisnurk(kujunditeArv)
+elif valik==2:
+    ring(kujunditeArv)
+elif valik==3:
+    ruut(kujunditeArv)
+else:
+    suvaline(0)
+
+turtle.done()
