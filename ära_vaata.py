d6ba77b6ada625f347ea0867952d3d9c849f4543
Alex Kreimann <alex@hkhk.edu.ee>
2025-01-08 14:38:51 +0200

    Update ära_vaata.py

---
+++ ära_vaata.py
@@ -0,0 +1,214 @@
+#Alex Kreimann
+#07.01.2025
+#Asi42
+
+#ÃœLESANNE 1 - Sisemine hÃ¤Ã¤l
+
+#KÃ•IK SUURTÃ„HTEDEGA KIRJUTAMINE ON NAGU KARJUMINE.
+#Vahel on parem kasutada oma â€žsisemist hÃ¤Ã¤lt" ja kirjutada tÃ¤ielikult vÃ¤ikeste tÃ¤htedega.
+
+#Failis nimega sisemine_haal.py loo programm Pythonis, mis kÃ¼sib kasutajalt sisendit ja vÃ¤ljastab selle sama sisendi vÃ¤ikeste tÃ¤htedega. KirjavahemÃ¤rgid ja tÃ¼hikud peavad jÃ¤Ã¤ma muutmata. Sul on vabadus, kuid mitte kohustus, lisada sisestuskÃ¤sule oma tekst, nÃ¤iteks andes input-ile enda sÃµne argumendiks.
+
+
+#ÃœLESANNE 2 - Taasesituse kiirus
+
+#MÃµnel inimesel on harjumus rÃ¤Ã¤kida vÃ¤ga kiiresti, ja vahel oleks tore neid veidi aeglustada â€“ nÃ¤iteks nagu YouTube'is kiirusega 0.75 vÃµi isegi pausidega sÃµnade vahel.
+
+#Failis nimega taasesitus.py loo programm Pythonis, mis kÃ¼sib kasutajalt sisendit ja vÃ¤ljastab selle sama sisendi, asendades iga tÃ¼hiku ... (ehk kolme punktiga).
+
+
+#ÃœLESANNE 3 - Einstein
+
+#Isegi kui sa pole fÃ¼Ã¼sikat (hiljuti vÃµi Ã¼ldse!) Ãµppinud, oled ilmselt kuulnud valemist E=mcÂ², kus:
+#
+#    E tÃ¤histab energiat (mÃµÃµdetuna dÅ¾aulides)
+#    m tÃ¤histab massi (mÃµÃµdetuna kilogrammides)
+#    c tÃ¤histab valguse kiirust (ligikaudu 300 000 000 meetrit sekundis)
+
+#Albert Einsteini ja tema kaasautorite jÃ¤rgi tÃ¤hendab see valem sisuliselt, et mass ja energia on ekvivalentsed.
+
+#Failis nimega einstein.py loo programm Pythonis, mis kÃ¼sib kasutajalt massi tÃ¤isarvuna (kilogrammides) ja vÃ¤ljastab ekvivalendi energia dÅ¾aulides tÃ¤isarvuna. Eelda, et kasutaja sisestab alati tÃ¤isarvu.
+
+
+#ÃœLESANNE 4 - Jootraha kalkulaator
+
+#Restoranis on tavaks jÃ¤tta teenindajale jootraha, mis moodustab tavaliselt vÃ¤hemalt 15% arve summast.
+
+#Programm kÃ¼sib kasutajalt summa sÃµnena formaadis ##.##â‚¬ (kus iga # tÃ¤histab numbrit), eemaldab euro mÃ¤rgi (â‚¬) ja teisendab summa ujukomaarvuks (float). NÃ¤iteks, kui kasutaja sisestab 50.00â‚¬, tagastab programm 50.0.
+
+#Eeldatakse, et kasutaja sisestab vÃ¤Ã¤rtused alati nÃµutud formaadis.
+
+#vaatab
+
+#ÃœLESANNE 5 - Deep Thought
+
+#    "All right," said the computer, and settled into silence again. The two men fidgeted. The tension was unbearable.
+#    "Youâ€™re really not going to like it," observed Deep Thought.
+#    "Tell us!"
+#    "All right," said Deep Thought. "The Answer to the Great Questionâ€¦"
+#    "Yesâ€¦!"
+#    "Of Life, the Universe and Everythingâ€¦" said Deep Thought.
+#    "Yesâ€¦!"
+#    "Isâ€¦" said Deep Thought, and paused.
+#    "Yesâ€¦!"
+#    "Isâ€¦"
+#    "Yesâ€¦!!!â€¦?"
+#    "Forty-two," said Deep Thought, with infinite majesty and calm."
+#â€” The Hitchhikerâ€™s Guide to the Galaxy, Douglas Adams
+
+#Failis deep.py loo programm, mis kÃ¼sib kasutajalt vastust Elu, universumi ja kÃµige suurele kÃ¼simusele.
+#    Kui kasutaja sisestab 42, forty-two vÃµi forty two (tÃµstutundetuseta), vÃ¤ljastab programm Yes.
+#    KÃµigil muudel juhtudel vÃ¤ljastab programm No.
+#Eeldatakse, et kasutaja sisestab vastuse tekstina vÃµi arvuna.
+
+
+#ÃœLESANNE 6 - Faililaiendid
+
+#uigi Windows ja macOS peidavad neid vahel, on enamikul failidel laiendid â€“ jÃ¤relliited, mis algavad punktiga (.) ja asuvad faili nime lÃµpus. NÃ¤iteks lÃµpevad GIF-failide nimed .gif ja JPEG-failide nimed .jpg vÃµi .jpeg laienditega. TopeltklÃµpsuga failil otsustab sinu arvuti selle laiendi pÃµhjal, millist programmi faili avamiseks kasutada.
+
+#Veebibrauserid seevastu kasutavad failide kuvamiseks meediatÃ¼Ã¼pe (varasemalt tuntud kui MIME-tÃ¼Ã¼bid). Faili allalaadimisel saadab veebiserver koos failiga HTTP pÃ¤ise, mis sisaldab faili meediatÃ¼Ã¼pi. NÃ¤iteks on GIF-faili meediatÃ¼Ã¼p image/gif ja JPEG-failil image/jpeg. MeediatÃ¼Ã¼bi mÃ¤Ã¤ramiseks vaatab veebiserver tavaliselt faili laiendit ja seostab selle vastava meediatÃ¼Ã¼biga.
+
+#Levinumate meediatÃ¼Ã¼pide kohta leiad infot:
+#developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+
+#Failis laiendid.py loo programm, mis kÃ¼sib kasutajalt faili nime ja vÃ¤ljastab selle faili meediatÃ¼Ã¼bi, kui faili nimi lÃµpeb (tÃµstutundetuseta) mÃµne jÃ¤rgmistest laienditest:
+
+#    .gif â†’ image/gif
+#    .jpg â†’ image/jpeg
+#    .jpeg â†’ image/jpeg
+#    .png â†’ image/png
+#    .pdf â†’ application/pdf
+#    .txt â†’ text/plain
+#    .zip â†’ application/zip
+
+#Kui faili nimi lÃµpeb mÃµne muu laiendiga vÃµi puudub laiend tÃ¤ielikult, vÃ¤ljastab programm:
+#application/octet-stream
+
+#Eeldatakse, et kasutaja sisestab failinime Ãµigesti vormindatuna.
+
+#vaatab
+
+#ÃœLESANNE 7 - Matemaatika
+
+#Python toetab juba matemaatilisi tehteid, vÃµimaldades liita, lahutada, korrutada ja jagada nii vÃ¤Ã¤rtusi kui ka muutujaid. Kuid kirjutame programmi, mis vÃµimaldab kasutajatel teha matemaatilisi tehteid ilma Pythonit tundmata.
+
+#Failis matem.py loo programm, mis kÃ¼sib kasutajalt aritmeetilise avaldise ja arvutab ning vÃ¤ljastab tulemuse ujukomaarvuna (float), vormindatuna Ã¼he kÃ¼mnendkohaga.
+
+#Eelda, et kasutaja sisestus on alati kujul: x y z
+#Kus:
+#    x on tÃ¤isarv
+#    y on Ã¼ks jÃ¤rgmistest sÃ¼mbolitest: +, -, *, /
+#    z on tÃ¤isarv
+
+#NÃ¤iteks, kui kasutaja sisestab:
+#1 + 1
+#peaks programm vÃ¤ljastama:
+#2.0
+
+#Eelda, et kui y on /, siis z ei ole kunagi 0.
+
+
+#ÃœLESANNE 8 - NÃ¤mNaÃ¤m
+
+#Oletame, et tavaks on sÃ¼Ã¼a:
+#    HommikusÃ¶Ã¶ki vahemikus 7:00 â€“ 8:00
+#    LÃµunasÃ¶Ã¶ki vahemikus 12:00 â€“ 13:00
+#    Ã•htusÃ¶Ã¶ki vahemikus 18:00 â€“ 19:00
+
+#Oleks ju tore, kui sul oleks programm, mis Ã¼tleb sulle, millal on aeg sÃ¼Ã¼a?
+#Failis sook.py loo programm, mis kÃ¼sib kasutajalt aega ja vÃ¤ljastab, kas on aeg hommikusÃ¶Ã¶giks, lÃµunasÃ¶Ã¶giks vÃµi ÃµhtusÃ¶Ã¶giks. Kui pole sÃ¶Ã¶gi aeg, ei vÃ¤ljasta programm midagi.
+
+#Eelda, et kasutaja sisestab aja 24-tunnises formaadis:
+
+    #:## vÃµi ##:##
+
+#Ja eelda, et iga sÃ¶Ã¶giaja vahemik on kaasav. NÃ¤iteks, kui aeg on 7:00, 7:01, 7:59 vÃµi 8:00, on aeg hommikusÃ¶Ã¶giks.
+#Programmi struktuur peaks olema jÃ¤rgmine:
+#    teisenda aeg (str) 24-tunnises formaadis vastavaks arvuks (float).
+#    NÃ¤iteks, kui antakse aeg "7:30", tagastab funktsioon 7.5.
+#    programm kontrollib aja alusel, kas on hommiku-, lÃµuna- vÃµi ÃµhtusÃ¶Ã¶gi aeg, ja vÃ¤ljastab vastava teate.
+	
+#vaatab
+	
+#ÃœLESANNE 9 - Coca-Cola automaat
+
+#Oletame, et automaat mÃ¼Ã¼b Coca-Cola pudeleid hinnaga 50 senti ning aktsepteerib ainult jÃ¤rgmisi mÃ¼ndivÃ¤Ã¤rtusi:
+#    25 senti
+#    10 senti
+#     5 senti
+
+#Failis coke.py loo programm, mis:
+#    KÃ¼sib kasutajalt korduvalt mÃ¼nti, teatades iga kord, kui palju raha on veel puudu.
+#    Kui kasutaja on sisestanud vÃ¤hemalt 50 senti, arvutab programm ja vÃ¤ljastab tagasiantava raha hulga sentides.
+#    Eeldatakse, et kasutaja sisestab alati tÃ¤isarve. Kui sisestatud vÃ¤Ã¤rtus ei ole 25, 10 ega 5, ignoreerib programm seda ja kÃ¼sib uut sisendit.
+
+
+#ÃœLESANNE 10 - twttr
+
+#SÃµnumite saatmisel vÃµi sÃ¤utsude kirjutamisel on Ã¼sna tavaline sÃµnu lÃ¼hendada, et sÃ¤Ã¤sta aega vÃµi ruumi, nÃ¤iteks jÃ¤ttes Ã¤ra tÃ¤ishÃ¤Ã¤likud. Samamoodi oli Twitter algselt tuntud kui twttr.
+
+#Failis twttr.py loo programm, mis kÃ¼sib kasutajalt teksti ja vÃ¤ljastab sama teksti, kuid eemaldab kÃµik tÃ¤ishÃ¤Ã¤likud (A, E, I, O, U) olenemata sellest, kas need on suured vÃµi vÃ¤iksed tÃ¤hed.
+
+
+#ÃœLESANNE 11 - NumbrimÃ¤rk ARK-is
+
+#ARK-is on vÃµimalik registreerida autonumbreid, kuid need peavad vastama kindlale vormingule:
+#    NumbrimÃ¤rk peab koosnema tÃ¤pselt 6 tÃ¤hemÃ¤rgist.
+#    Esimesed kolm mÃ¤rki peavad olema suurtÃ¤hed (A-Z).
+#    Viimased kolm mÃ¤rki peavad olema numbrid (0-9).
+#    Muud tÃ¤hemÃ¤rgid, tÃ¼hikud vÃµi kirjavahemÃ¤rgid ei ole lubatud.
+
+#Loo programm, mis:
+#    KÃ¼sib kasutajalt kohandatud autonumbri.
+#    Kontrollib, kas see vastab nÃµuetele.
+#    VÃ¤ljastab:
+#        SOBIB, kui numbrimÃ¤rk vastab vormingule AAA123.
+#        EI SOBI, kui see ei vasta nÃµuetele.
+
+
+#ÃœLESANNE 11 - KÃ¼tusenÃ¤idik
+
+#KÃ¼tusenÃ¤idikud nÃ¤itavad sageli murdarvude abil, kui palju kÃ¼tust on paagis. NÃ¤iteks:
+#    1/4 nÃ¤itab, et paak on 25% tÃ¤is.
+#    1/2 nÃ¤itab, et paak on 50% tÃ¤is.
+#    3/4 nÃ¤itab, et paak on 75% tÃ¤is.
+
+#Failis kytus.py loo programm, mis:
+#    KÃ¼sib kasutajalt murdosa kujul X/Y, kus X ja Y on tÃ¤isarvud.
+#    Arvutab ja vÃ¤ljastab kÃ¼tusekoguse protsendina (Ã¼mardatuna lÃ¤hima tÃ¤isarvuni).
+#    Kui kÃ¼tust on 1% vÃµi vÃ¤hem, vÃ¤ljastab E (empty â€“ tÃ¼hi).
+#    Kui kÃ¼tust on 99% vÃµi rohkem, vÃ¤ljastab F (full â€“ tÃ¤is).
+#    Kui kasutaja sisestus ei ole korrektne:
+#        X vÃµi Y ei ole tÃ¤isarv
+#        X on suurem kui Y
+#        Y on 0
+#        â€“ kÃ¼sib programm sisestust uuesti.
+#Programmis tuleb kasutada try-except veapÃ¼Ã¼dmist, et vÃ¤ltida programmi krahhi vigase sisendi korral.
+
+
+#ÃœLESANNE 12 - Ostukorv
+
+#Oletame, et sul on harjumus koostada nimekiri toidupoe ostudest.
+
+#Failis ostukorv.py loo programm, mis:
+#    KÃ¼sib kasutajalt esemeid Ã¼he kaupa.
+#    Kasutaja saab sisestamise lÃµpetada, kui sisestus on tÃ¼hi.
+#    Kuvab lÃµpus ostunimekirja:
+#        KÃµik esemed kuvatakse SUURTÃ„HTEDEGA.
+#        Nimekiri on sorteeritud tÃ¤hestikulises jÃ¤rjekorras.
+#        Iga eseme ees on arv, mis nÃ¤itab, mitu korda kasutaja seda eset sisestas.
+#    Kasutaja sisestusi kÃ¤sitletakse tÃµstutundetult (case-insensitive).
+
+#NÃ¤ide sisendist:
+#Ãµun  
+#banaan  
+#Ãµun  
+#leib  
+#piim  
+#Ãµun  
+# 
+#VÃ¤ljund:
+#3 Ã•UN  
+#1 BANAAN  
+#1 LEIB  
+#1 PIIM  
